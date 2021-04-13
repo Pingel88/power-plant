2cc6e35d223ee912b287b83bf825c6565eb1d9e3
Michael Pingel <mdpingel@gmail.com>
2021-04-13 13:04:45 -0700

    add backend code to plant.js
    Co-authored-by: Jon Stump <jmstump@gmail.com>

---
+++ src/js/plant.js
@@ -0,0 +1,75 @@
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+// This is a fucntion factor. We can easily create more specific functions that alter a plan's soul, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+// We create four functions using our function faction. We could easily create many more.
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+const brawndo = changeState("water")(20);
+
+
+
+
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
+
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// };
+
+// const changeState = (state, prop, value) => {
+//   return {
+//     ...state,
+//     [prop]: (state[prop] || 0) + value
+//   }
+// };
+
+
+// const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//       ...state,
+//       [prop]: (state[prop] || 0) + value
+//     })
+//   }
+// };
+
+// const storeState = () => {
+//   let currentState = {};
+//   return (stateChangeFunction = state => state) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = {...newState};
+//     return newState;
+//   }
+// }
