4adb0418d91433eadd6568a737d3b4c528ea6ba8
Michael Pingel <mdpingel@gmail.com>
2021-04-13 14:13:08 -0700

    create working DOM
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- src/js/plant.js
+++ src/js/plant.js
@@ -9,11 +9,11 @@ const storeState = () => {
   }
 }
 
-const stateControl = storeState();
+export const stateControl = storeState();
 
 // This is a fucntion factor. We can easily create more specific functions that alter a plan's soul, water, and light to varying degrees.
 
-const changeState = (prop) => {
+export const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
@@ -21,55 +21,3 @@ const changeState = (prop) => {
     })
   }
 }
-
-// We create four functions using our function factory. We could easily create many more.
-
-const feed = changeState("soil")(1);
-const blueFood = changeState("soil")(5);
-
-const hydrate = changeState("water")(1);
-const superWater = changeState("water")(5);
-const brawndo = changeState("water")(20);
-
-
-
-
-// const hydrate = (plant) => {
-//   return {
-//     ...plant,
-//     water: (plant.water || 0) + 1
-//   }
-// };
-
-// const feed = (plant) => {
-//   return {
-//     ...plant,
-//     soil: (plant.soil || 0) + 1
-//   }
-// };
-
-// const changeState = (state, prop, value) => {
-//   return {
-//     ...state,
-//     [prop]: (state[prop] || 0) + value
-//   }
-// };
-
-
-// const changeState = (prop) => {
-//   return (value) => {
-//     return (state) => ({
-//       ...state,
-//       [prop]: (state[prop] || 0) + value
-//     })
-//   }
-// };
-
-// const storeState = () => {
-//   let currentState = {};
-//   return (stateChangeFunction = state => state) => {
-//     const newState = stateChangeFunction(currentState);
-//     currentState = {...newState};
-//     return newState;
-//   }
-// }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './js/plant.js'
+import {stateControl, changeState} from './js/plant.js';
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+const brawndo = changeState("water")(20);
 
 // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
 
@@ -16,5 +23,5 @@ $('#feed').click(function() {
 $('#show-state').click(function() {
   // We just need to call stateControl() without arguments to see our current state.
   const currentState = stateControl();
-  $('#soil-value').text(`Soil: ${currentState.soil}`);
+  $('#current-value').text(`Soil: ${currentState.soil}`);
 });
